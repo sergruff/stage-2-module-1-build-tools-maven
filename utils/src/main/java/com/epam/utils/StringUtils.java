335d380fb32bb73253769c887a3345e7ef6e7fdb
sergruff <sergruff@gmail.com>
2023-03-11 23:51:32 +0100

    s2m1taskBuildToolsMaven

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,16 @@ package com.epam.utils;
 import java.lang.*;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Integer.parseInt(str) >= 0;
+        if (str==null) {
+            return false;
+        }
+        try {
+            return Double.parseDouble(str)>0;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+
+
     }
 }
